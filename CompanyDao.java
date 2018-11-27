fc5090debdd2c27285601135da83c976e93c9dc9
Krzysiek Snarski <ksnarski1968@gmail.com>
2018-11-27 22:30:22 +0100

    Jest to commid ktory przeniesiemy

---
+++ CompanyDao.java
@@ -0,0 +1,6 @@
+and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other grea
+
+
+
+
+
